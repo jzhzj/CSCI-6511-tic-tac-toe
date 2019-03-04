d9fcc1e95ba88a5fbbf41b240796febbfd62c236
Jiuzhi Qi <35633477+jzhzj@users.noreply.github.com>
2019-03-04 15:14:15 -0500

    update project properties

--- src/main/java/edu/gwu/seas/ai/team6/util/ProjProps.java
+++ src/main/java/edu/gwu/seas/ai/team6/util/ProjProps.java
@@ -7,7 +7,33 @@ public class ProjProps {
         ResourceBundle rb = ResourceBundle.getBundle("prop");
 
         SERVER_ADDR = rb.getString("SERVER_ADDR");
+        X_API_KEY = rb.getString("X_API_KEY");
+        USER_ID = rb.getString("USER_ID");
+        CONTENT_TYPE = rb.getString("CONTENT_TYPE");
+        BODY_TYPE_TEAM = rb.getString("BODY_TYPE_TEAM");
+        BODY_TYPE_MEMBER = rb.getString("BODY_TYPE_MEMBER");
+        BODY_TYPE_GAME = rb.getString("BODY_TYPE_GAME");
+        BODY_TYPE_MOVE = rb.getString("BODY_TYPE_MOVE");
+        BODY_GAME_TYPE_TTT = rb.getString("BODY_GAME_TYPE_TTT");
+        PRAMS_TYPE_TEAM = rb.getString("PRAMS_TYPE_TEAM");
+        PRAMS_TYPE_MOVES = rb.getString("PRAMS_TYPE_MOVES");
+        PRAMS_TYPE_BOARD_STRING = rb.getString("PRAMS_TYPE_BOARD_STRING");
+        PRAMS_TYPE_BOARD_MAP = rb.getString("PRAMS_TYPE_BOARD_MAP");
+        TEAM_ID = rb.getString("TEAM_ID");
     }
 
     public static final String SERVER_ADDR;
+    public static final String X_API_KEY;
+    public static final String USER_ID;
+    public static final String CONTENT_TYPE;
+    public static final String BODY_TYPE_TEAM;
+    public static final String BODY_TYPE_MEMBER;
+    public static final String BODY_TYPE_GAME;
+    public static final String BODY_TYPE_MOVE;
+    public static final String BODY_GAME_TYPE_TTT;
+    public static final String PRAMS_TYPE_TEAM;
+    public static final String PRAMS_TYPE_MOVES;
+    public static final String PRAMS_TYPE_BOARD_STRING;
+    public static final String PRAMS_TYPE_BOARD_MAP;
+    public static final String TEAM_ID;
 }
